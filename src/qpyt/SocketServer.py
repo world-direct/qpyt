459f1964915f0eb3f77a3ee2502ef4f47c66aff6
guenter.prossliner <6724584+gprossliner@users.noreply.github.com>
2026-01-12 12:57:27 +0100

    enhance SocketServer logging for IP binding and add get_local_ip_addresses function closes #17

--- src/qpyt/SocketServer.py
+++ src/qpyt/SocketServer.py
@@ -1,6 +1,8 @@
-import socketserver
-from qpyt.ComPort import ComPort
 import logging
+import socket
+import socketserver
+
+from qpyt.ComPort import ComPort
 
 log = logging.getLogger(__name__)
 
@@ -54,7 +56,13 @@ class SocketServer:
         with socketserver.TCPServer((self.listen_ip, self.listen_port), SocketServer.TCPHandler) as server:
             # Activate the server; this will keep running until you
             # interrupt the program with Ctrl-C
-            log.info(f"Started socket server on port {self.listen_port}, forwarding to {self.port_name}")
+            if self.listen_ip == "0.0.0.0":
+                ips = get_local_ip_addresses()
+                log.info("Started socket server on all interfaces, forwarding to {}".format(self.port_name))
+                for ip in ips:
+                    log.info(f" - {ip}:{self.listen_port}")
+            else:
+                log.info(f"Started socket server on {self.listen_ip}:{self.listen_port}, forwarding to {self.port_name}")
             server.ss = self
             server.serve_forever()
 
@@ -70,3 +78,26 @@ class SocketServer:
         log.debug(data.decode())
 
         pass
+
+def get_local_ip_addresses():
+    ip_list = []
+    hostname = socket.gethostname()
+    def is_link_local(ip):
+        return ip.startswith('169.254.')
+    try:
+        # This gets the primary IP
+        ip = socket.gethostbyname(hostname)
+        if not is_link_local(ip):
+            ip_list.append(ip)
+    except Exception:
+        pass
+    # This gets all IPs
+    for info in socket.getaddrinfo(hostname, None):
+        ip = info[4][0]
+        if ip not in ip_list and ':' not in ip:  # skip IPv6 for now
+            if not is_link_local(ip):
+                ip_list.append(ip)
+    # Add 127.0.0.1 explicitly
+    if '127.0.0.1' not in ip_list:
+        ip_list.append('127.0.0.1')
+    return ip_list
